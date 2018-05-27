99971493b10b0e1040c4fca0eb455d0d4d6de22e
scriptype <scriptyper@gmail.com>
2018-05-28 01:06:49 +0300

    Conditionally display signup & login / logout links in base

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, IndexLink } from 'react-router'
+import Auth from '../modules/Auth'
 
 const Base = ({ children }) => (
   <div>
@@ -9,10 +10,16 @@ const Base = ({ children }) => (
         <IndexLink to="/">Cuttlesoft - React Challenge</IndexLink>
       </div>
 
-      <div className="top-bar-right">
-        <Link to="/signup">Sign up</Link>
-        <Link to="/login">Login</Link>
-      </div>
+      { Auth.isUserAuthenticated() ? (
+        <div className="top-bar-right">
+          <Link to="/logout">Logout</Link>
+        </div>
+      ) : (
+        <div className="top-bar-right">
+          <Link to="/signup">Sign up</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      ) }
 
     </div>
 
